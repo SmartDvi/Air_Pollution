0721bc022f53f3e7520bf48091a561835ffe27ae
SmartDvi <your.petersmoritus@gmail.comgit config --global user.name SmartDvigit config --global user.email your.petersmoritus@gmail.com>
2024-09-11 09:34:16 +0100

    Add Mantine stylesheets and theme switching functionality

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ from utils import melted_df
 
 _dash_renderer._set_react_version("18.2.0")
 
-app = Dash(__name__, use_pages=True, external_stylesheets=dmc.styles.ALL)
+stylesheets = [
+    "https://unpkg.com/@mantine/dates@7/styles.css",
+    "https://unpkg.com/@mantine/code-highlight@7/styles.css",
+    "https://unpkg.com/@mantine/charts@7/styles.css",
+    "https://unpkg.com/@mantine/carousel@7/styles.css",
+    "https://unpkg.com/@mantine/notifications@7/styles.css",
+    "https://unpkg.com/@mantine/nprogress@7/styles.css",
+]
+
+app = Dash(__name__, 
+           use_pages=True, 
+           external_stylesheets=stylesheets,
+           suppress_callback_exceptions=True,
+           prevent_initial_callbacks=False,
+           )
 
 # developing the icon and theme for changing theme color
 themes_toggle = dmc.ActionIcon(
@@ -40,7 +54,7 @@ header = dmc.Group(
 
 )
 # developing the side setup inside a variable 
-navbar = dcc.dcc.Loading(
+navbar = dcc.Loading(
     dmc.ScrollArea(
         [
             dmc.NavLink(label='Introduction and Project Detail',),
@@ -58,17 +72,69 @@ navbar = dcc.dcc.Loading(
     )
 )
 
+page_content = [
+    dash.page_container
+]
+
+app_shell = dmc.AppShell(
+    [
+        dmc.AppShellHeader(header, px=25),
+        dmc.AppShellNavbar(navbar, p=24),
+        dmc.AppShellMain(page_content),
+        dmc.AppShellFooter(
+            [
+                dmc.Group(
+                    [
+                        dmc.NavLink(
+                            label= 'Sources Code',
+                            description='double Sources',
+                            leftSection=dmc.Badge(
+                                "2", size="xs", variant='filled', color='orange', w=16, h=16,p=0
+                            ),
+                            childrenOffset=28,
+                            children=[
+                                dmc.NavLink(label="GitHub", href='https://github.com/SmartDvi/Air_Pollution.git'),
+                                dmc.NavLink(label="GitHub", href='https://py.cafe/SmartDvi/plotly-global-air-quality')
+                            ]
+
+                        )
+                    ], justify='lg'
+                )
+            ]
+        )
+    ]
+),
+id='app_shell'
 
 # developing app layout
-app.layout = dbc.Container(
-    dbc.Col(
-        [
-            dash.page_container
-        ]
-    )
+app.layout = dmc.MantineProvider(
+    [
+        dcc.Store(id='theme_store', storage_type='local', data='light'),
+        app_shell
+    ],
+    id='mantine_provider',
+    forceColorScheme='light',
 )
 
+@callback(
+    Output('app_shell', 'navbar'),
+    Input('burger-icon', 'opened'),
+    State('app_shell', 'navbar')
+)
 
+def navbar_is_open(opened, navbar):
+    navbar['collapsed'] = {'mobile': not opened}
+    return navbar
+
+@callback(
+    Output('mantine_provider', 'forceColorScheme'),
+    Input('toggle-color', 'n_click'),
+    State('mantine_provider', 'forceColorScheme'),
+    prevent_initial_call = True,
+)
+
+def switch_theme(_, theme):
+    return 'dark' if theme == 'light' else 'light'
 
 
 
